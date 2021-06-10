0b40895f186aa21b83d2da102063164a75ca23e8
Galip İrice <galipirice@gmail.com>
2021-06-11 00:53:53 +0300

    more work aded

--- src/test/java/Class1one.java
+++ src/test/java/Class1one.java
@@ -2,5 +2,6 @@ public class Class1one {
     public static void main(String[] args) {
         System.out.println("HelloTest");
         System.out.println("Helloooooo");
+        System.out.println("More work");
     }
 }
