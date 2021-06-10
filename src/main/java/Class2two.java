1bcd78d0f44cff1bf581b9a883ee4e4b076eaa65
Galip İrice <galipirice@gmail.com>
2021-06-11 00:37:50 +0300

    initial commit

---
+++ src/main/java/Class2two.java
@@ -0,0 +1,7 @@
+public class Class2two {
+
+    public static void main(String[] args) {
+        System.out.println("Hello"
+        );
+    }
+}
---
+++ src/test/java/Class1one.java
@@ -0,0 +1,5 @@
+public class Class1one {
+    public static void main(String[] args) {
+        System.out.println("HelloTest");
+    }
+}
